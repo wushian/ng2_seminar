89829f4d5e99f49ea9f563859dd56d767ee08290
Bibby <bibbynet@gmail.com>
2016-02-16 00:59:30 +0800

    add "demo2_helloWorldJs"

---
+++ demo2_helloWorldJs/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+    <h1>My First Angular 2 -- Hello World</h1>
+    <hr />
+    <h3>.js Files</h3>
+    `
+})
+export class AppComponent { }
